6038a80e5d5fe82696e2beb6c9ddde8a4568deca
beckyshaz <sharonsakah8@gmail.com>
2024-01-18 06:30:10 -0800

    program to print number and list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    i = len(argv) - 1
+    if i < 1:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
+    for x in range(i):
+        print("{}: {:s}".format(x + 1, argv[x + 1])) 
+            
+        
