698ced0117e90fb48b57b98294fd780ecf562bd9
beckyshaz <sharonsakah8@gmail.com>
2024-02-09 06:40:38 -0800

    linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ *check_cycle - function that checks if there is a circle in a linked list
+ *@list: linked list to be checked
+ *Return: 1 if there is a circle else 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *temp;
+
+	temp = list;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		if (temp == list)
+			return (1);
+	}
+	return (0);
+}
