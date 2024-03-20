7ba4441e059458f7561f05aa5f3e8bdd947e35ed
beckyshaz <sharonsakah8@gmail.com>
2024-03-20 05:13:19 -0700

    removing key value pairs by value in a dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,5 @@
 #!/urs/bin/python3
 def complex_delete(a_dictionary, value):
     if value in a_dictionary:
-        new_dicts = {key: val for key, val in a_dictionary.items() if val != value}
-        return new_dicts
-    else:
-        return a_dictionary
+        return {key: val for (key, val) in a_dictionary.items() if val != value}
+    return a_dictionary
