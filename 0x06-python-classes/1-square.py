c9e271a75c9e7630da8798fd16f03a092d9f79b2
beckyshaz <sharonsakah8@gmail.com>
2024-04-02 09:10:10 -0700

    initialising a private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""define a class square with
+size size"""
+
+
+class Square:
+    """defines a square
+    of class square"""
+
+    def __init__(self, size):
+        """Initialising a square
+        of size size.
+        Args:
+        size(int): The size of
+        the square"""
+        self.__size = size
