2302c5296946f886fd68f4e3daac87babe5a2110
beckyshaz <sharonsakah8@gmail.com>
2024-04-02 05:45:48 -0700

    typeerror raised

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except (TypeError, ValueError):
         print("Exception: Unknown format code 'd' for object of type 'str'", file=sys.stderr)
         return False
          
