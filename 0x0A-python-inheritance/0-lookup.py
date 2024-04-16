2e312c4502963be1348a1338b8332fc6c3e049b5
beckyshaz <sharonsakah8@gmail.com>
2024-04-15 19:01:06 -0700

    dir function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function to return a list of methods and attributes
+of a class"""
+
+
+
+def lookup(obj):
+    """"Defining a function that returns alist of
+    attributes and methods of a class using dir"""
+    return dir(obj)
