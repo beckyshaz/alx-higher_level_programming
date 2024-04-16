becd4ac21356490156e41145ee4abc98ae39b80a
beckyshaz <sharonsakah8@gmail.com>
2024-04-15 20:10:05 -0700

    inheritance checking

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""checking for inheritance directly or indirectly"""
+
+
+def inherits_from(obj, a_class):
+    """Defining a function that checks for inheritance"""
+    return isinstance(obj, a_class)
