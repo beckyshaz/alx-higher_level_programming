69a2b5042113e0fa6b0ea73cd48b33db12c46d58
beckyshaz <sharonsakah8@gmail.com>
2024-04-17 07:49:45 -0700

    changing a dictionary with other data type into json representation

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,9 +4,13 @@ simple data structure for JSON serialization
 of an object"""
 
 
-import json
-
-
 def class_to_json(obj):
     """serializing object called obj"""
-    return json.dumps(obj)
+    if isinstance(obj, dict):
+        return {key: class_to_json(value) for key, value in obj.items()}
+    elif isinstance(obj, (list, tuple)):
+        return [class_to_json(item) for item in obj]
+    elif isinstance(obj, (str, int, bool)):
+        return obj
+    else:
+        return str(obj)
