e7ea229b0e6d2ea7836ca3ff9ec6ee04399d29de
beckyshaz <sharonsakah8@gmail.com>
2024-05-22 07:02:24 -0700

    creating user nd granting all privilages

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- creating  user in  database
+-- the user should be granted all privilages
+CREATE USER  IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
