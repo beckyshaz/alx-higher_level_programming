6e51020d9d8d1d5834fc09239a16f644c7224bfb
beckyshaz <sharonsakah8@gmail.com>
2024-06-25 06:35:31 -0700

    mysql through pyton script

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,19 +4,27 @@
 
 
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    try:
+        db = MySQLdb.connect(
             host="localhost",
-            password="root",
-            user="root",
-            database="hbtn_0e_0_usa",
+            password=password,
+            user=username,
+            database=dbname,
             port=3306
             )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    myresult = cur.fetchall()
-    for row in myresult:
-        print(row)
-    cur.close()
-    db.close()
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        myresult = cur.fetchall()
+        for row in myresult:
+            print(row)
+        cur.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print(f"Error {e}")
+        sys.exit(1)
