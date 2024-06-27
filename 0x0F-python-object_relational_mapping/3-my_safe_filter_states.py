b93cde1124f1e8edfab47f0fd0b4a7e3e2ed705f
beckyshaz <sharonsakah8@gmail.com>
2024-06-27 07:34:57 -0700

    query safe from sqlinjection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values
+in the states able of hbtn_0e_0_usa where name matches
+the argument"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    state_name = sys.argv[4]
+
+    try:
+        db = MySQLdb.connect(
+                host="localhost",
+                user=username,
+                password=password,
+                database=dbname,
+                port=3306,
+                charset="utf8"
+                )
+        cur = db.cursor()
+        query = """
+        SELECT *
+        FROM states
+        WHERE name LIKE BINARY %s
+        ORDER BY id ASC
+        """
+        cur.execute(query, (state_name,))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+                                          
