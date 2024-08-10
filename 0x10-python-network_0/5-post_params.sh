7a65486d9825fc72bdc0dd6ad374546828f8f4b1
beckyshaz <sharonsakah8@gmail.com>
2024-08-10 06:36:56 -0700

    post request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # post request
-curl -X  POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
+curl -X  POST -d "email=test@gmail.comsubject=I will always be here for PLD" "$1"
